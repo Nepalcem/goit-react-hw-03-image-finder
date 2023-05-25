ec5997bd113f882912fe28216ab2c8c5e37c6f51
Nepalcem <mark.emailtest@gmail.com>
2023-05-26 00:53:53 +0300

    full components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import SearchBar from './SearchBar/SearchBar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import LoadMoreBtn from './LoadMoreBtn/LoadMoreBtn';
 // import { ToastContainer, toast } from 'react-toastify';
+import { ProgressBar } from 'react-loader-spinner';
+// import Modal from './Modal/Modal';
 
 export default class App extends Component {
   render() {
@@ -12,6 +14,19 @@ export default class App extends Component {
         <SearchBar></SearchBar>
         <ImageGallery></ImageGallery>
         <LoadMoreBtn></LoadMoreBtn>
+        <ProgressBar
+          height="80"
+          width="80"
+          ariaLabel="progress-bar-loading"
+          wrapperStyle={{
+            display: 'block',
+            margin: '0 auto',
+          }}
+          wrapperClass="progress-bar-wrapper"
+          borderColor="#F4442E"
+          barColor="#51E5FF"
+        />
+        {/* <Modal></Modal> */}
       </div>
     );
   }
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { OverlayStyled } from './Overlay.styled';
+import { ModalStyled } from './Modal.styled';
+
+export default function Modal() {
+  return (
+    <OverlayStyled>
+      <ModalStyled>
+        <img src="" alt="" />
+      </ModalStyled>
+    </OverlayStyled>
+  );
+}
