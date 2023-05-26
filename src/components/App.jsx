e2b80fccea784a9c4f31f857699dca13ed3a0481
Nepalcem <mark.emailtest@gmail.com>
2023-05-26 18:53:15 +0300

    firsth fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,34 @@ import LoadMoreBtn from './LoadMoreBtn/LoadMoreBtn';
 // import { ToastContainer, toast } from 'react-toastify';
 import { ProgressBar } from 'react-loader-spinner';
 // import Modal from './Modal/Modal';
+import { fetchImagesWithQuery } from './services/api';
 
 export default class App extends Component {
+  state = {
+    formInput: null,
+    images: [],
+  };
+
+  getFormInput = ({ input }) => {
+    this.setState({ formInput: input });
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    if (prevState.formInput !== this.state.formInput) {
+      try {
+        const imagesArr = await fetchImagesWithQuery(this.state.formInput);
+        this.setState({ images: imagesArr });
+        console.log(this.state.images);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
   render() {
     return (
       <div>
-        <SearchBar></SearchBar>
+        <SearchBar onSubmit={this.getFormInput}></SearchBar>
         <ImageGallery></ImageGallery>
         <LoadMoreBtn></LoadMoreBtn>
         <ProgressBar
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import { SearchBarStyled } from './SearchBar.styled';
 
 export default class SearchBar extends Component {
+  state = {
+    input: '',
+  };
+
+  handleChange = e => {
+    const { value } = e.currentTarget;
+    this.setState({ input: value.trim() });
+  };
+  submitHandler = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.setState(prevState => ({ ...prevState, input: '' }));
+  };
+
   render() {
     return (
       <SearchBarStyled>
-        <form className="form">
+        <form className="form" onSubmit={this.submitHandler}>
           <button type="submit" className="button">
             <span className="button-label">Search</span>
           </button>
@@ -16,6 +30,8 @@ export default class SearchBar extends Component {
             autoComplete="off"
             autoFocus
             placeholder="Search images and photos"
+            onChange={this.handleChange}
+            value={this.state.input}
           />
         </form>
       </SearchBarStyled>
